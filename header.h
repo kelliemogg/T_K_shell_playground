3936f87fe4b74da590e180a7ba29de254e64b5af
Kellie Mogg <kelliedmogg@gmail.com>
2020-11-15 19:20:10 +0000

    combined our headers

--- header.h
+++ header.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -5,3 +8,17 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+
+char _strchr(char *s, char c);
+int fork_process(void);
+int stringlength(char *s);
+char *read_line(void);
+
+char *envp[] =
+{
+	"HOME=/",
+	"PATH=/bin:usr/bin",
+0
+};
+
+#endif /* HEADER_H */
